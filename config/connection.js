ab044a887d6a242f197ed4fb8293f1642387fc69
Dan Beccaria <beccdaniel@gmail.com>
2019-08-07 17:07:23 -0400

    added jawsdb config

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql")
-
-        connection = mysql.createConnection({
+let connection;
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
         host:"localhost",
         port:3306,
         user: "root",
@@ -8,14 +11,9 @@ const mysql = require("mysql")
         database:"burgers_db"
     
     })
-
-
-connection.connect((err) => {
-    if(err) {
-        console.log(`You got an error ${err}`)
+}
        
-    }
-    console.log(`You're connected as ${connection.threadId}`)
-})
+
+connection.connect()
 
 module.exports = connection;
